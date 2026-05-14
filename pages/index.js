eb2d2a94b5def97773fb2940c892aa2f21d2d426
Maksat <maksatylyas@gmail.com>
2026-05-15 00:26:32 +0500

    Change homepage text

--- pages/index.js
+++ pages/index.js
@@ -54,9 +54,9 @@ export default function Index() {
       <Hero initialValues={hero_data} />
       <Top_Destinations initialValues={top_destinations_data} />
       <Start_About initialValues={start_about_data} />
+      <Perfect_Travel_Planner initialValues={perfect_travel_planner_data} />
       <Popular_Tours initialValues={popular_tours_data} />
       <Full_Video initialValues={full_video_data} />
-      <Perfect_Travel_Planner initialValues={perfect_travel_planner_data} />
       <Why_Choose_Us initialValues={why_choose_us_data} />
       <Start_Testimonial initialValues={start_testimonial_data} />
       <Latest_Blog_News initialValues={latest_blog_news_data} />
