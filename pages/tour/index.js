5a4ab80510d725a6b6c32f0ebf3baef2f11eccf1
Maksat <maksatylyas@gmail.com>
2026-06-24 17:52:29 +0500

    changed the json tour file to supabase

--- pages/tour/index.js
+++ pages/tour/index.js
@@ -3,23 +3,36 @@ import Header from "@/Layout/Header";
 import { Comman_Hero } from "@/component/Sections/Page-commen";
 import { All_Tour } from "@/component/Sections/Page-tour";
 import { Head_Meta, useFetchData } from "@/component/comman";
+import { getPublishedTours } from "../../lib/getPublishedTours";
 import React from "react";
-export default function Tour() {
+
+export default function Tour({ tours }) {
   const { data: hero_tour_data } = useFetchData("json/data/hero_tour.json");
 
-  const { data: tour_product_data } = useFetchData(
-    "json/data/tour_product.json"
-  );
-
   // Fetch Seo data
-  const { data: seo_data } = useFetchData("/json/data/site_meta_link.json"); // Fetch Seo data using the custom hook
+  const { data: seo_data } = useFetchData("/json/data/site_meta_link.json");
 
   return (
     <>
-      {/* Render the Seo component with SeoData */}
       <Head_Meta meta_data={seo_data.tour_meta} comman_meta={seo_data} />
       <Comman_Hero initialValues={hero_tour_data} />
-      <All_Tour initialValues={tour_product_data} />
+
+      <div style={{ padding: "30px", background: "#fff", color: "#000" }}>
+        Loaded tours: {tours?.length || 0}
+      </div>
+
+      <All_Tour initialValues={tours} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const tours = await getPublishedTours();
+
+  return {
+    props: {
+      tours,
+    },
+    revalidate: 60,
+  };
 }
