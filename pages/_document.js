1b9a4850e1f3944c632728fbf765e82b517873dd
Maksat <maksatylyas@gmail.com>
2026-07-31 15:53:11 +0500

    Self host Red Hat Display font

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,19 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Red_Hat_Display } from "next/font/google";
+
+const redHatDisplay = Red_Hat_Display({
+  subsets: ["latin"],
+  display: "swap",
+  preload: true,
+});
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="anonymous"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+      <Html lang="en" className={redHatDisplay.className}>
+        <Head />
 
-        <body className="text-md md:text-lg font-red-hat-display antialiased text-dark-800 leading-xl">
+        <body className="text-md md:text-lg antialiased text-dark-800 leading-xl">
           <Main />
           <NextScript />
         </body>
